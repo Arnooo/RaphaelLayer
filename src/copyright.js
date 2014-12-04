87b84ad6bcbcbf75f5b0543e8aae20d6ed0709f2
Arno <arnaud.joubel@gmail.com>
2014-12-04 20:07:18 +0100

    Description:Up version 0.3.3
    Issue: NA
    Author: Arnaud Joubel <arnaud.joubel@gmail.com>
    Review: NA
    Test performed:
    Test result: pass

--- src/copyright.js
+++ src/copyright.js
@@ -2,7 +2,7 @@
  RaphaelLayer, a JavaScript library for overlaying Raphael objects onto Leaflet interactive maps. http://dynmeth.github.com/RaphaelLayer
  (c) 2012-2013, David Howell, Dynamic Methods Pty Ltd
 
- Version 0.3.2
+ Version 0.3.3
 
  Fork by Arnooo: https://github.com/Arnooo/RaphaelLayer
 
